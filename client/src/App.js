5aeaa5e01800d3ef81b098055ef485f80fd1bc12
MatthewRJue <matthewjue101@gmail.com>
2024-02-19 22:35:36 -0500

    added list

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 import Navbar from './Components/Navbar';
 import React from 'react';
+import MovieList from "./Components/MovieList"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update this line
 
 function App() {
@@ -11,7 +12,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={
-          <Navbar />
+          <><Navbar />
+          <MovieList />
+          </>
         } />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
