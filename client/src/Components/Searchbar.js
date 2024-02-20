136ff515b0d2c2e472a7f6a102b159765f84e990
liamfenn <lgf63569@uga.edu>
2024-02-20 11:33:38 -0500

    Added Filter button to search bar

--- client/src/Components/Searchbar.js
+++ client/src/Components/Searchbar.js
@@ -1,5 +1,6 @@
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-import { Link } from 'react-router-dom'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { Link } from 'react-router-dom';
+import Filter from './Filter';
 
 export default function Searchbar() {
     return (
@@ -14,8 +15,11 @@ export default function Searchbar() {
             name="movie"
             id="movie"
             className="block w-full rounded-md border-0 py-1.5 pl-10 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder="Search Movies..."
+            placeholder="Search Movies by Name, Genre, etc..."
           />
+          <span className="sm:ml-3">
+            <Filter />
+          </span>
           <span className="sm:ml-3">
             <Link to="/" className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
               Search
