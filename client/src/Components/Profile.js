8f7e25134fc5740108a6c29ea270b11708262b0f
liamfenn <lgf63569@uga.edu>
2024-03-28 19:28:36 -0400

    Fixed Profile Email

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -183,7 +183,6 @@ export default function Profile() {
                     id="email"
                     name="email"
                     value={userInfo.email}
-                    onChange={handleUserInfoChange}
                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   />
                 </div>
