31ffdc5a235d454613e1de178cdd0374d642cd60
MatthewRJue <matthewjue101@gmail.com>
2024-04-01 14:32:12 -0400

    removed admin profile option

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -69,12 +69,16 @@ export default function Navbar({status, updateAdminTab, onLogout}) {
             Logout
           </button>
         </span>
+        </div>}
+        { status==="User" &&
+        <div className="mt-0 flex lg:ml-4 lg:mt-0">
         <span className="sm:ml-3">
           <Link to="/profile" className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Profile
           </Link>
         </span>
       </div>}
+
     </div>
   )
 }
